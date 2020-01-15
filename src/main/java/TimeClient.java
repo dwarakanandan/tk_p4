fc3e1d6c47715b03486fef8620b1232d915dd97d
dwaarakanandan <dwarakanandan.bm@gmail.com>
2020-01-15 22:39:57 +0100

    Implemented NTP Server and Client

--- src/main/java/TimeClient.java
+++ src/main/java/TimeClient.java
@@ -21,10 +21,14 @@ public class TimeClient {
 			for (int i = 0; i < 10; i++) {
 				NTPRequest ntpRequest = new NTPRequest();
 				socket = new Socket(InetAddress.getByName(hostUrl), PORT);
+
+				// Send an NTP Request by setting the current client time as T1 in the ntpRequest
 				long ntpRequestTimestamp = System.currentTimeMillis();
 				ntpRequest.setT1(ntpRequestTimestamp);
 				System.out.println(CLIENT_TAG + "Sending NTP Request at: " + ntpRequestTimestamp + "\n");
 				sendNTPRequest(ntpRequest);
+
+				// Receive the NTP Response and the current client time as T4
 				ObjectInputStream ntpResponseInputStream = new ObjectInputStream(socket.getInputStream());
 				try {
 					ntpRequest = (NTPRequest) ntpResponseInputStream.readObject();
@@ -34,19 +38,32 @@ public class TimeClient {
 				long ntpResponseTimestamp = System.currentTimeMillis();
 				ntpRequest.setT4(ntpResponseTimestamp);
 				System.out.println(CLIENT_TAG + "Got NTP Response at: " + ntpResponseTimestamp + "\n");
+				
+				// We now have an NTPRequest object with all 4 values T1,T2,T3,T4 set.
+				// Calculate Offset and Delay
 				ntpRequest.calculateOandD();
 				System.out.println(CLIENT_TAG + "Iteration: " + i + " Estimated offset = " + ntpRequest.o + " , Delay = " + ntpRequest.d + "\n");
+				
+				// If the current m and m' pair has the minimum overall delay. Cache it
 				if ( (i == 0) || ntpRequest.d < this.minD) {
 					this.minD = ntpRequest.d;
 					this.minNTPrequest = ntpRequest;
 				}
+
 				socket.close();
+
+				// Wait 350 milliseconds before sending the next request
 				threadSleep(350);
 			}
 			socket.close();
 
 			System.out.println(CLIENT_TAG + "Estimated offset corresponding to minimum delay = " + this.minNTPrequest.o + " , Minumum Delay = " + this.minNTPrequest.d + "\n");
 
+			if (this.minNTPrequest.o >= 0) {
+				System.out.println(CLIENT_TAG + "Server is approximately " + this.minNTPrequest.o + " milliseconds ahead of self\n");
+			} else {
+				System.out.println(CLIENT_TAG + "Server is approximately " + this.minNTPrequest.o + " milliseconds behind self\n");
+			}
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -56,9 +73,11 @@ public class TimeClient {
 
 	private void sendNTPRequest(NTPRequest request) {
 		try {
+			// Artificially induced communication delay of 10 - 100 milli seconds
 			Random random = new Random();
 			threadSleep(10 + random.nextInt(90));
 
+			// Send the NTP request object in which T1 is already set
 			ObjectOutputStream objectOutputStream = new ObjectOutputStream(this.socket.getOutputStream());
 			objectOutputStream.writeObject(request);
 			objectOutputStream.flush();
--- src/main/java/TimeServer.java
+++ src/main/java/TimeServer.java
@@ -15,6 +15,8 @@ public class TimeServer {
 		try {
 			serverSocket = new ServerSocket(PORT);
 			System.out.println("Server started on port: " + PORT);
+
+			// Keep the server running indefnitely and delegate any new  NTP requests to the Executor Thread Pool
 			while (true) {
 				NTPRequestHandler ntpRequestHandler = new NTPRequestHandler(serverSocket.accept());
 				ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(4);
@@ -51,6 +53,7 @@ public class TimeServer {
 			this.client = client;
 		}
 
+		// Induce an artificial offset of 1100 milliseconds on the server
 		private long getCurrentServerTime() {
 			return System.currentTimeMillis() + 1100L;
 		}
@@ -59,11 +62,17 @@ public class TimeServer {
 		public void run() {
 			
 			try {
+				// Receive the NTPRequest sent by the client and set the current server time as T2
 				ObjectInputStream objectInputStream = new ObjectInputStream(this.client.getInputStream());
 				NTPRequest ntpRequest = (NTPRequest) objectInputStream.readObject();
 				long ntpRequestTimestamp = getCurrentServerTime();
 				ntpRequest.setT2(ntpRequestTimestamp);
 				System.out.println("[Thread-" + Thread.currentThread().getId() + "]" + SERVER_TAG + "Got NTP Request at :"+ ntpRequestTimestamp + "\n");
+				
+				// Set the current server time as T3 and send the NTP response
+				long ntpResponseTimestamp = getCurrentServerTime();
+				ntpRequest.setT3(ntpResponseTimestamp);
+				System.out.println("[Thread-" + Thread.currentThread().getId() + "]" + SERVER_TAG + "Sending NTP Response at :"+ ntpResponseTimestamp + "\n");
 				sendNTPAnswer(ntpRequest);
 			} catch (IOException | ClassNotFoundException e) {
 				e.printStackTrace();
@@ -73,13 +82,12 @@ public class TimeServer {
 
 		private void sendNTPAnswer(NTPRequest request) {
 			try {
+				// Artificially induced communication delay of 10 - 100 milli seconds
 				Random random = new Random();
 				threadSleep(10 + random.nextInt(90));
 
+				// Send the NTP request object in which T1,T2,T3 is already set
 				ObjectOutputStream objectOutputStream = new ObjectOutputStream(this.client.getOutputStream());
-				long ntpResponseTimestamp = getCurrentServerTime();
-				request.setT3(ntpResponseTimestamp);
-				System.out.println("[Thread-" + Thread.currentThread().getId() + "]" + SERVER_TAG + "Sending NTP Response at :"+ ntpResponseTimestamp + "\n");
 				objectOutputStream.writeObject(request);
 				objectOutputStream.flush();
 			} catch (IOException e) {
